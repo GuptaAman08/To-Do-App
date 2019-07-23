2af6475cb2d98a7d300c54e2cd8ad4455c823c5e
GuptaAman08 <31820923+GuptaAman08@users.noreply.github.com>
2019-07-23 09:27:24 +0530

    Adding items to list part is done, deletion part remaining

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+TO-DO-List
+This is a web application developed using NODE, EXPRESS, template engine EJS and MONGODB.
+User can add, delete to-do operations flexibly.
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var todoController = require('./controllers/todoController')
+
+// set up the express app
+var app = express()
+
+// set up the template Engine
+app.set('view engine', 'ejs')
+
+//to serve static files
+app.use(express.static('./public'))
+
+// fire controllers
+todoController(app)
+
+// Listen to port 
+app.listen(3000)
+console.log('Listening to port 3000........')
